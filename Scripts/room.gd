7dc3f39e87a431f4a60b5ea84e8d1559da8bb338
NextLevel <calvinjtenny@icloud.com>
2024-06-04 10:22:45 -0700

    Partial code for rebuilding Wall2 doorframe
    Added code to position and resize top of Wall2 doorframe and fixed bugs in generate_collisions

--- Scripts/room.gd
+++ Scripts/room.gd
@@ -10,6 +10,7 @@ class_name Room
 @onready var wall_3_mesh = $Wall3
 @onready var wall_4_mesh = $Wall4
 @onready var wall_1_node = $Wall1Node
+@onready var wall_2_node = $Wall2Node
 
 @export var dimensions: Vector3 = Vector3(5,5,5)
 
@@ -113,14 +114,41 @@ func rebuild():
 	else:
 		wall_1_mesh.show()
 		wall_1_node.hide()
+	
+	if wall_2_door:
+		# Replacing the base wall mesh with a more complex group of meshes for the door frame
+		wall_2_mesh.hide()
+		get_node("Wall2Node").show()
+		
+		# Positioning the frame along the z axis
+		$Wall2Node.position.z = -((dimensions.z - 0.5) / 2)
+		
+		# Resizing the top of the frame
+		$Wall2Node/Top.mesh.size.y = dimensions.y - 3.5
+		$Wall2Node/Top.mesh.size.x = dimensions.x
+		# Positioning the top of the frame
+		$Wall2Node/Top.position.y = (dimensions.y - (($Wall2Node/Top.mesh.size.y / 2)) - 0.5) - 2.5
+		
+		# Resizing the side of the frame
+		#$Wall1Node/Side1.mesh.size.z = (dimensions.z - 1.5) / 2
+		# Positioning the side of the frame
+		#$Wall1Node/Side1.position.z = ((-dimensions.z - 1.5) / 2) / 2
+		
+		# Resizing the other side of the frame
+		#$Wall1Node/Side2.mesh.size.z = (dimensions.z - 1.5) / 2
+		# Positioning the other side of the frame
+		#$Wall1Node/Side2.position.z = ((dimensions.z + 1.5) / 2) / 2
+	else:
+		wall_2_mesh.show()
+		get_node("Wall2Node").hide()
 
 func generate_collisions():
 	if wall_1_door:
 		for mesh in wall_1_node.get_children():
 			var collision: CollisionPolygon3D = CollisionPolygon3D.new()
-			collision.polygon = mesh.create_trimesh_collision()
+			collision.polygon = mesh.create_trimesh_shape()
 			wall_1_node.add_child(collision)
 	else:
 		var collision: CollisionPolygon3D = CollisionPolygon3D.new()
-		collision.polygon = wall_1_mesh.create_trimesh_collision()
+		collision.polygon = wall_1_mesh.create_trimesh_shape()
 		wall_1_mesh.add_child(collision)
